ee695678def3ae7f994992a1f7930d3928fee40a
Nihar <nihar1kashyap7@gmail.com>
2022-08-23 11:28:18 +0530

    Endpoints for job submit and fetch done

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, request
 from collections import Counter
+from rq import Queue
+from redis import Redis
+from rq.job import Job
+
+conn = Redis('localhost', 6379, charset="utf-8")
+
+q = Queue(connection=conn)
 
 app = Flask(__name__)
 
 def count_chars(word):
     count = Counter(word)
+    print(count)
     return count
 
 
@@ -15,4 +23,15 @@ def hello_world():
 
 @app.route("/count", methods=["POST"])
 def count_words():
-    return {'word':request.form['word']}
+    result = q.enqueue(count_chars, request.form['word'])
+    print(result.get_id())
+    return {'count':'result'}
+
+@app.route("/result", methods=["GET"])
+def get_result():
+    args = request.args.to_dict()
+    job = Job.fetch(args.get("job_key"), connection=conn)
+    if job.is_finished:
+        return job.result, 200
+    else:
+        return "Nay!", 202
